ce478fffef369c23f00a2680deb70d59455949cf
mohamed-badr555 <mohamedb.555dr@gmail.com>
2024-09-06 20:50:16 +0300

    Edit animation

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,7 +9,7 @@ import { fadeIn,textVariant } from '../utils/motion';
 
 const ProjectCard =({index , name, description,tags,image,source_code_link}) =>{
   return (
-    <motion.div  variants={fadeIn("left","spring",index*0.5,0.75)}>
+    <motion.div  variants={fadeIn("down","spring",index*0.5,0.75)}>
       <Tilt options={{max:45,scale:1,speed:450}} className="bg-tertiary p-5 
       rounded-2xl sm:w-[360px] w-full h-full flex flex-col justify-between  ">
         <div className="relative w-full h-[230px] ">
