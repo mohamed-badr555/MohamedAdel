72a96cac195d8e5725a11d560087748044cb5e42
mohamed-badr555 <mohamedb.555dr@gmail.com>
2025-01-10 01:05:55 +0200

    Add Houref Platform edit error

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,8 @@
 import {Tilt} from  'react-tilt'
 import {motion} from 'framer-motion'
 import {styles} from '../styles'
-import { github } from '../assets';
+import { github, liveDemo } from '../assets'; // Updated import
 import {SectionWrapper} from '../hoc';
-import {liveDemo} from '../assets'
 import {projects} from '../constants'
 import { fadeIn,textVariant } from '../utils/motion';
 import { Link } from 'react-router-dom';
