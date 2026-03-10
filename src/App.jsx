e872ff04b8cfc5c485bdac4204c5c00fc7e8d9e8
Mohamed Adel <mohamedb.555dr@gmail.com>
2026-03-10 04:09:31 +0200

    Remove Feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter  } from "react-router-dom"
-import {About, Contact, Experience , Feedbacks, Hero, Navbar, Tech ,Works,StarsCanvas } from './components';
+import {About, Contact, Experience , Hero, Navbar, Tech ,Works,StarsCanvas } from './components';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
       <Experience/>
       <Tech/>
       <Works/>
-      <Feedbacks/>
+   
       <div className="relative z-0">
         <Contact/>
         <StarsCanvas/>
