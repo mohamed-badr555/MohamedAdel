c027ff8a1f487470289bc01d3326d4cd0053548b
mohamed-badr555 <mohamedb.555dr@gmail.com>
2026-03-11 13:37:56 +0200

    tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,37 @@
+import { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(() => window.matchMedia('(max-width: 640px)').matches);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 640px)');
+    const handler = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener('change', handler);
+    return () => mediaQuery.removeEventListener('change', handler);
+  }, []);
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
- {technologies.map((technologie) => (
-  <div className="w-28 h-28 " key={technologie.icon}>
-    <BallCanvas icon={technologie.icon} />
-  </div>
- ))}
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          {isMobile ? (
+            <div className="w-full h-full flex items-center justify-center rounded-full bg-tertiary border border-white/10 shadow-md shadow-[#915EFF]/10 p-5">
+              <img
+                src={technology.icon}
+                alt={technology.name}
+                className="w-full h-full object-contain"
+              />
+            </div>
+          ) : (
+            <BallCanvas icon={technology.icon} />
+          )}
+        </div>
+      ))}
     </div>
   )
 }
 
-export default SectionWrapper( Tech,"")
+export default SectionWrapper(Tech, "")
